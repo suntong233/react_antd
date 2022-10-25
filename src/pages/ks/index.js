17fa78f4f1c87eb77ae504ab765006e6de03cf32
yi_bei_lin <2654073941@qq.com>
2022-10-25 16:03:35 +0800

    s

--- src/pages/ks/index.js
+++ src/pages/ks/index.js
@@ -4,13 +4,95 @@
 import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import styles from "./index.module.scss";
-
-
-
+let IMG = require.context("../../images/ks", true, /\.png$|\.jpg$/);
+let IMGKeys = IMG.keys();
 const Main = () => {
+    const [fightInfo, setFightInfo] = useState({});
+    
+
     let {search} = useLocation();
 
     useEffect(() => {
+        setFightInfo({
+            playerData: {
+                2654073941: {
+                    name: "playerName",
+                    key: "playerid",
+                    uData: {
+                        skill:{skill1:true},
+                        "hp": {
+                            "头部": {
+                                "now": 100,
+                                "max": 100
+                            },
+                            "胸部": {
+                                "now": 100,
+                                "max": 100
+                            },
+                            "腹部": {
+                                "now": 100,
+                                "max": 100
+                            },
+                            "左手": {
+                                "now": 100,
+                                "max": 100
+                            },
+                            "右手": {
+                                "now": 100,
+                                "max": 100
+                            },
+                            "左腿": {
+                                "now": 100,
+                                "max": 100
+                            },
+                            "右腿": {
+                                "now": 100,
+                                "max": 100
+                            }
+                        },
+                        "shuxin": {
+                            "力量": 205286,
+                            "敏捷": 53573,
+                            "韧性": 114208,
+                            "武士刀": 73,
+                            "军刀": 1,
+                            "砍刀": 1,
+                            "长柄刀": 1,
+                            "钝器": 248195,
+                            "重武器": 69285,
+                            "攻击": 262493,
+                            "防御": 182833,
+                            "偷窃": 8762,
+                            "撬锁": 4881
+                        },
+                        "equip": {
+                            "腰带": "提灯",
+                            "头部": "锡罐(杰作)",
+                            "盔甲": "圣国铠甲(专家)",
+                            "背心": "黑色链甲(杰作)",
+                            "腿部": "",
+                            "鞋子": "",
+                            "武器": "重型十手(MKⅠ)"
+                        },
+                    },
+                    def: false, // 
+                    isUser: true,
+                    playerCamp: true, // 
+                    taunt: false, // 
+                    zb: 50, // 
+                }
+            },
+            npcData: {
+                npc1: {
+                    name: "npcname",
+                    key: "npckey",
+                    uData: {}, // 
+                    def: false, // 
+                    taunt: false, // 
+                    zb: 50, // 
+                }
+            }
+        });
         // 获取路由携带数据
         if(search && search.length>0) {
             let query = {};
@@ -35,10 +117,107 @@ const Main = () => {
                 <div className={`${styles.title} h3`}>ks</div>
             </div>
             <div className={styles.content}>
+                {
+                    fightInfo?.playerData ? 
+                        <div id="puppeteerScreenShortFightInfo" className={styles.fightInfoContainer}>
+                            {renderFightCard(fightInfo.playerData? fightInfo.playerData[2654073941]:{})}
+                        </div>
+                    : null
+                }
                 
             </div>
         </div>
     )
 }
 
+function calcHp(hpInfo) {
+    let scale = (hpInfo.now/hpInfo.max)*100;
+    let now = Math.round(hpInfo.now);
+    let max = Math.round(hpInfo.max);
+    let text = `${now}/${max}`
+    return {
+        scale,text
+    }
+}
+
+function renderFightCard(props) {
+    if(!props || !props.uData){return <></>}
+    let {def,taunt,playerCamp,name,key,uData,isUser,head} = props // zb isUser
+    let {hp,race,equip,shuxin,skill,cd} = uData // skill cd equip
+    return <div className={styles.fightInfoCard}>
+        <div className={styles.fightInfoCardLeftBox}>
+            <div className={styles.fightInfoCardLeftBoxBaseInfo}>
+                <div className={styles.fightInfoCardLeftBoxBaseInfoHeadIcon}>
+                    <img alt="head" src={/http/.test(head)? head: getImgPath(head)}></img>
+                </div>
+                <div className={styles.fightInfoCardLeftBoxBaseInfoHeadFontBox}>
+                    <div>{name}</div>
+                    <div>{key}</div>
+                    <div>{def?"防御 ":""} {taunt?"嘲讽 ":""} {race||"人类"}</div>
+                    <div>{playerCamp?"是":"非"}玩家队伍</div>
+                </div>
+            </div>
+            <div className={styles.fightInfoCardLeftBoxEquipBox}>
+                {
+                    Object.values(equip).filter(v=>v!=="").join("  ")
+                }
+            </div>
+            <div className={styles.fightInfoCardLeftBoxEquipBox}>
+                {
+                    Object.entries(shuxin).filter(v => /攻击|防御|力量|敏捷|韧性/.test(v[0])).map(v=>{return `${v[0]}:${isUser? xp2lv(v[1]):v[1]}`}).join(" ")
+                }
+            </div>
+            <div className={styles.fightInfoCardLeftBoxEquipBox}>
+                {
+                    Object.keys(skill||{}).join(" ")
+                }
+                {
+                    Object.keys(cd||{}).join(" ")
+                }
+            </div>
+        </div>
+        <div className={styles.fightInfoHpBox}>
+            <div className={styles.fightInfoHpBoxBuwei}>
+                <div style={{"width": `${calcHp(hp["头部"]).scale}%`}} className={styles.fightInfoHpBoxBuweihp}></div>
+                <div className={styles.fightInfoHpBoxBuweihpFont}> {`头部 ${calcHp(hp["头部"]).text}`} </div>
+            </div>
+            <div className={styles.fightInfoHpBoxBuwei}>
+                <div style={{"width": `${calcHp(hp["胸部"]).scale}%`}} className={styles.fightInfoHpBoxBuweihp}></div>
+                <div className={styles.fightInfoHpBoxBuweihpFont}> {`胸部 ${calcHp(hp["胸部"]).text}`} </div>
+            </div>
+            <div className={styles.fightInfoHpBoxBuwei}>
+                <div style={{"width": `${calcHp(hp["腹部"]).scale}%`}} className={styles.fightInfoHpBoxBuweihp}></div>
+                <div className={styles.fightInfoHpBoxBuweihpFont}> {`腹部 ${calcHp(hp["腹部"]).text}`} </div>
+            </div>
+            <div className={styles.fightInfoHpBoxBuwei}>
+                <div style={{"width": `${calcHp(hp["左手"]).scale}%`}} className={styles.fightInfoHpBoxBuweihp}></div>
+                <div className={styles.fightInfoHpBoxBuweihpFont}> {`左手 ${calcHp(hp["左手"]).text}`} </div>
+            </div>
+            <div className={styles.fightInfoHpBoxBuwei}>
+                <div style={{"width": `${calcHp(hp["右手"]).scale}%`}} className={styles.fightInfoHpBoxBuweihp}></div>
+                <div className={styles.fightInfoHpBoxBuweihpFont}> {`右手 ${calcHp(hp["右手"]).text}`} </div>
+            </div>
+            <div className={styles.fightInfoHpBoxBuwei}>
+                <div style={{"width": `${calcHp(hp["左腿"]).scale}%`}} className={styles.fightInfoHpBoxBuweihp}></div>
+                <div className={styles.fightInfoHpBoxBuweihpFont}> {`左腿 ${calcHp(hp["左腿"]).text}`} </div>
+            </div>
+            <div className={styles.fightInfoHpBoxBuwei}>
+                <div style={{"width": `${calcHp(hp["右腿"]).scale}%`}} className={styles.fightInfoHpBoxBuweihp}></div>
+                <div className={styles.fightInfoHpBoxBuweihpFont}> {`右腿 ${calcHp(hp["右腿"]).text}`} </div>
+            </div>
+        </div>
+    </div>
+}
+function xp2lv(xp) {
+    // Math.floor(10000**0.4/2.5)
+    return Math.floor(xp ** 0.28 / 0.48)
+}
+function getImgPath(name) {
+    let findP = IMGKeys.find(v => new RegExp(`/${name}.(png|jpg)$`).test(v))
+    if(IMGKeys.includes(findP)) {
+        return IMG(findP)
+    } else {
+        return IMG("./kenshi.jpg")  // 
+    }
+}
 export default Main;
