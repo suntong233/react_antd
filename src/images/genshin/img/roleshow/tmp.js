05adc5166d90375f74ea84e2ddd439a87064f808
18361172461 <scott.sun@topibd.com>
2023-10-23 15:26:48 +0800

    s

--- src/images/genshin/img/roleshow/tmp.js
+++ src/images/genshin/img/roleshow/tmp.js
@@ -2,16 +2,18 @@
 
 let fs = require("fs");
 let constants = fs.constants;
-let dir = "D:/vsCodeWork/scott/pro/miao-plugin/resources/meta/character"
-let names = fs.readdirSync("D:/vsCodeWork/scott/pro/miao-plugin/resources/meta/character")
+let dir = "D:/vsCodeWork/scott/pro/Miao-Yunzai/plugins/miao-plugin/resources/meta/character"
+let names = fs.readdirSync(dir)
 
 
 for (let index = 0; index < names.length; index++) {
     let name = names[index];
     let imgPath = dir + `/${name}/imgs/splash.webp`;
+    let cardPath = dir + `/${name}/imgs/card.webp`;
     try {
         fs.accessSync(imgPath, constants.R_OK | constants.W_OK);
-        fs.copyFileSync(imgPath, `./${name}.webp`)
+        fs.copyFileSync(imgPath, `./img/${name}3.png`)
+        fs.copyFileSync(cardPath, `./img/${name}1.png`)
     } catch (err) {
         console.error('no access!' + name);
     }
